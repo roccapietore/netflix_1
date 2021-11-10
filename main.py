e113115e985c491dd8514b28718d7927423c2a2b
Verevkina Elena <verevkinaee@gmail.com>
2021-11-10 11:05:01 +0200

    add route /cast

--- main.py
+++ main.py
@@ -55,10 +55,41 @@ def get_movies_by_genre():
 
 @app.route("/search")
 def search_by_parameters():
-    sql = f"SELECT title, description FROM netflix WHERE type =  '', release_year = '', listed_in = '' LIMIT 10"
+    sql_start = "select title, description from netflix where "
+    sql_lst = []
+    sql = ''
+    if type_ := request.args.get('type'):
+        sql_lst.append(f" type = '{type_}'")
+    if year := request.args.get('year'):
+        sql_lst.append(f" release_year = '{year}'")
+    if genre := request.args.get('genre'):
+        sql_lst.append(f" listed_in like '%{genre}%'")
+    if sql_lst:
+        sql = sql_start + ' and '.join(sql_lst)
+        result = get_data_base(sql)
+        return jsonify(json_format('title', 'description', data_base=result))
+
+
+@app.route("/cast/<actor_1>/<actor_2>")
+def get_actors(actor_1: str, actor_2: str):
+    actor_list = []
+    new_list = []
+    sql = f"SELECT `cast` FROM netflix WHERE `cast` LIKE '%{actor_1}%{actor_2}%' or `cast` like '%{actor_2}%{actor_1}%'"
     result = get_data_base(sql)
-    return jsonify(json_format('title', 'description', data_base=result))
+    for cast in result:
+        for actor in cast:
+            actor = actor.split(', ')
+            actor_list += actor
+            if actor_1 in actor_list and actor_2 in actor_list:
+                actor_list.remove(actor_1)
+                actor_list.remove(actor_2)
+
+    for actor in actor_list:
+        if actor_list.count(actor) > 2:
+            new_list.append(actor)
+            new_list = list(set(new_list))
+    return jsonify(new_list)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5003)
